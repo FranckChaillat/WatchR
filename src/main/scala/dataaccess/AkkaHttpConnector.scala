a7cd2c825f35c90d712771353cf8c09e5d7b25ee
Chaillat Franck <f.chaillat@lectra.com>
2020-11-29 21:00:42 +0100

    fix

--- src/main/scala/dataaccess/AkkaHttpConnector.scala
+++ src/main/scala/dataaccess/AkkaHttpConnector.scala
@@ -1,7 +1,8 @@
 package dataaccess
 import akka.http.scaladsl.HttpExt
+import akka.http.scaladsl.model.headers.RawHeader
 import org.json4s.jackson.Serialization.{read, write}
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpMethods, HttpRequest, Uri}
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpHeader, HttpMethods, HttpRequest, Uri}
 import akka.stream.Materializer
 import akka.util.ByteString
 import org.json4s.Formats
@@ -19,11 +20,13 @@ class AkkaHttpConnector(http: HttpExt)(implicit formats: Formats, materializer: 
   }
 
   override def get[U <: AnyRef](uri: String, queryParams: Map[String, String])(implicit mf: Manifest[U]): Future[U] = {
-    val u = Uri(uri).withRawQueryString(queryParams.map(e => s"${e._1}=${e._2}").mkString(","))
+    val u = Uri(s"""$uri?${queryParams.map(e => s"${e._1}=${e._2}").mkString("&")}""")
     val request =  HttpRequest(uri = u, method = HttpMethods.GET)
     http.singleRequest(request)
       .flatMap { result =>
-        result.entity.dataBytes.runFold(ByteString(""))(_ ++ _).map(c  => read[U](c.toString()))
+        result.entity.dataBytes.runFold(ByteString(""))(_ ++ _).map(c  => {
+          read[U](c.utf8String)
+        })
       }
   }
 
