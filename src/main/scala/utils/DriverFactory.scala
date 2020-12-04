42201c1da0db640c41c52b2625d2cadc6f9881a5
Chaillat Franck <f.chaillat@lectra.com>
2020-12-04 14:30:10 +0100

    test

--- src/main/scala/utils/DriverFactory.scala
+++ src/main/scala/utils/DriverFactory.scala
@@ -22,7 +22,7 @@ class DriverFactory(driverPath: String) {
   def close() = {
     this.driver.foreach(d => { d.close(); d.quit() })
     this.driver = None
-    Runtime.getRuntime.exec("pkill --signal TERM -f chrome")
+    Runtime.getRuntime.exec("pkill --signal TERM -f /snap/bin/chromium")
     ()
   }
 
