b20d2d4fbca9d542cfca5fc2d56b90635bf2622a
Chaillat Franck <f.chaillat@lectra.com>
2020-12-04 15:40:41 +0100

    test

--- src/main/scala/utils/DriverFactory.scala
+++ src/main/scala/utils/DriverFactory.scala
@@ -29,7 +29,7 @@ class DriverFactory(driverPath: String) {
   private def buildDriver(): ChromeDriver = {
     System.setProperty("webdriver.chrome.driver", driverPath)
     val options = new ChromeOptions()
-    options.addArguments("--disable-gpu", "--headless", "--ignore-certificate-errors", "--blink-settings=imagesEnabled=false", "--start-maximized")
+    options.addArguments("--disable-gpu", "--headless", "--ignore-certificate-errors"/*, "--blink-settings=imagesEnabled=false", "--start-maximized"*/)
     val driver = new ChromeDriver(options)
     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS)
     driver
